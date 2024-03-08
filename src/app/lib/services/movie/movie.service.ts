a5868587c0f42851f987a0259f4ef942af934404
joy210 <sohanoor404@gmail.com>
2024-03-08 22:14:44 +0600

    upddate

--- src/app/lib/services/movie/movie.service.ts
+++ src/app/lib/services/movie/movie.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { environment } from '../../../../environments/environment.development';
 import { MovieDetails } from '../../interfaces/movie';
 
