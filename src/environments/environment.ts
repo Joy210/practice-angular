6a58b97d0ea3c5eca20f18bf57bf1b0e90e69551
Sohanur Rahman Joy <sohanoor404@gmail.com>
2024-03-08 22:10:16 +0600

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apikey: 'f6bced9437e6cceeeb154f47316b6427',
-  apiUrl: 'https://api.themoviedb.org/3/discover/movie',
-  movieGenreUrl: 'https://api.themoviedb.org/3/genre/movie/list',
-  token:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmJjZWQ5NDM3ZTZjY2VlZWIxNTRmNDczMTZiNjQyNyIsInN1YiI6IjYwODQwNTFkZmZjOWRlMDA1OGRiZTk4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ec5q0ui1bJbR4p7_Trh3fMzmt9POyeZuwhgMTjYVUOQ',
-  imgUrl: 'https://image.tmdb.org/t/p/w500',
-};
