a7d4cd4a16def9bddbc9d0d4de8a706a8cef660f
Joy210 <sohanoor404@gmail.com>
2024-04-01 12:16:03 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './pages/movies/movie-list/movie-list.component';
+import { PipelineComponent } from './pages/pipeline/pipeline.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MovieListComponent,
+  },
+  {
+    path: 'pipeline',
+    component: PipelineComponent,
   },
 
   // {
